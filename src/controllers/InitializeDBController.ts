112d10dfc080eff31dd74c86ed0fae6f50c5c6d3
root <root@DESKTOP-K6G9NKE.localdomain>
2023-09-12 23:10:07 +0200

    schema error

---
+++ src/controllers/InitializeDBController.ts
@@ -0,0 +1,69 @@
+import { Request, Response, NextFunction } from 'express';
+const Sequelize = require('sequelize');
+import fs from 'fs';
+import Artist from '../model/Artist';
+import Record from '../model/Record';
+
+async function initializeDB() {
+    const rootCert = fs.readFileSync('/etc/ssl/certs/ca-certificates.crt');
+
+    const sequelize = new Sequelize(process.env.DB_ROOT_NAME, process.env.DB_ROOT_USERNAME, process.env.DB_ROOT_PASSWD, {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                ca: rootCert, // Use the root certificate
+            }
+        }
+    });
+
+    await sequelize.query(
+        `DROP DATABASE IF EXISTS ${process.env.DB_NAME};`
+    );
+
+    await sequelize.query(
+        `CREATE DATABASE ${process.env.DB_NAME};`
+    );
+
+    await sequelize.query(
+        `DROP USER IF EXISTS ${process.env.DB_USERNAME};`
+    );
+
+    await sequelize.query(
+        `CREATE USER ${process.env.DB_USERNAME} WITH PASSWORD '${process.env.DB_PASSWD}';`
+    );
+
+    await sequelize.query(
+        `GRANT ALL PRIVILEGES ON DATABASE ${process.env.DB_NAME} TO ${process.env.DB_USERNAME};`
+    )
+
+    // Fermeture de la connexion admin
+    await sequelize.close();
+
+    //creation des tables
+    await Record.sync();
+    await Artist.sync();
+
+    console.log("db init done")
+
+}
+
+const InitializeDBController = {
+    async initializeDBAsEndpoint(req: Request, res: Response, next: NextFunction) {
+        try {
+
+            await initializeDB();
+
+            return res.status(200).json("done");
+
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json("error");
+
+        }
+    },
+
+}
+
+export default InitializeDBController;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import RecordController from '../controllers/RecordController'
 import { validateCreateRecord } from '../controllers/RecordController';
+import InitializeDBController from '../controllers/InitializeDBController';
 
 const router = express.Router();
 
 router.get('/records', RecordController.getRecords);
 router.post('/record', validateCreateRecord, RecordController.createRecord);
+router.post('/init-db', validateCreateRecord, InitializeDBController.initializeDBAsEndpoint);
 
 export default router
